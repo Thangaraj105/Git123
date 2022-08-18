5c446be1e236758a4c6acc07e2465e1487a30599
Thangaraj105 <thangarajr2@gmail.com>
2022-08-18 01:51:35 -0700

    firstgit

---
+++ src/org/gittest/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package org.gittest;
+
+public class EmployeeDetails {
+	
+	private void empName() {
+	
+		System.out.println("thangaraj");
+		}
+	private void empid() {
+	System.out.println("115260");
+	}
+public static void main(String[] args) {
+	EmployeeDetails e = new EmployeeDetails();
+	
+	e.empName();
+	e.empid();
+	
+}
+}
